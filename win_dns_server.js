d4530f49fd13bc263e2c93af5e02f0c7c3f54ea6
Burak Doğan <bdogan85@gmail.com>
2015-02-03 18:19:03 +0200

    First Initialize

---
+++ win_dns_server.js
@@ -0,0 +1,13 @@
+
+module.exports.index = function(req, res, next, args) {
+  global.db.server.find("servers", function(err, result){
+    res.json(result);
+  });
+};
+
+module.exports.sd = function(req, res, next, args) {
+  var zonefile = require('dns-zonefile');
+  var text = require('fs').readFileSync('./rpc_modules/win_dns_server/dns_out.txt', 'utf8');
+  output = zonefile.parse(text);
+  res.json(output);
+};
