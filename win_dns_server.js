b69861953105ef2b4de71922e1fcdfcae7ca4c78
Burak Doğan <bdogan85@gmail.com>
2015-02-04 17:54:34 +0200

    deneme

--- win_dns_server.js
+++ win_dns_server.js
@@ -9,6 +9,5 @@ module.exports.sd = function(req, res, next, args) {
   var zonefile = require('dns-zonefile');
   var text = require('fs').readFileSync('./rpc_modules/win_dns_server/dns_out.txt', 'utf8');
   output = zonefile.parse(text);
-  var a = 2;
   res.json(output);
 };
