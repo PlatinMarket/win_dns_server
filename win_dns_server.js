0b6e31bfd203e29b0a8a2450c331ded476c18a1f
Burak Doğan <me@burak-dogan.com>
2015-02-05 19:56:04 +0200

    Bug Fix

--- win_dns_server.js
+++ win_dns_server.js
@@ -6,6 +6,6 @@ module.exports.index = function(req, res, next, args) {
 module.exports.sd = function(req, res, next, args) {
   var zonefile = require('dns-zonefile');
   var text = require('fs').readFileSync(global.path('/rpc_modules/win_dns_server/dns_out.txt', 'utf8'));
-  output = zonefile.parse(text);
+  output = zonefile.parse(text.toString());
   res.json(output);
 };
