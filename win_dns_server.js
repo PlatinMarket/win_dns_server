2292038954f27ac3a78b1c89604d075061368801
Burak Doğan <me@burak-dogan.com>
2015-02-05 22:48:00 +0200

    deneme

--- win_dns_server.js
+++ win_dns_server.js
@@ -15,19 +15,22 @@ module.exports.index = function(req, res, next, args) {
   * List Of Dns Zones
   */
 module.exports.zones = function(req, res, next, args) {
-  global.execute(dnscmd, ["/EnumZones"], {},
+  global.execute(dnscmd, ["/aaaa"], {},
     function (error, stdout, stderr){
-      if (error) return res.status(500).end(JSON.stringfy(error));
-      var zones = stdout.toString().split("\r\n");
       
-      var out = [];
-      var up_shift = 0;
+      if (error) return res.status(500).end(JSON.stringfy(error));
+      
+      var zones = stdout.toString().split("\r\n"),
+          out = [],
+          up_shift = 0;
+
       for (var i in zones) {
         if (i > 2 && zones[i].slice(0, 1) == " " && zones[i - (up_shift + 3)].trim().slice(0, 9) == "Zone name") {
           up_shift = up_shift + 1;
           out.push(zones[i].trim().replace(/ .*/gi, ""));
         }
       }
+
       return res.json(out);
     }
   );
