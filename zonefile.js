f5a76c43516b315cf44e93bc19d75ce025268b7c
Burak Doğan <bdogan85@gmail.com>
2015-02-11 16:27:17 +0200

    final Parser

--- zonefile.js
+++ zonefile.js
@@ -180,9 +180,9 @@
             } else if (uRR.indexOf(('Server:').toUpperCase()) === 3) {
                 ret.server = rr.split(/\s+/g)[2];
             } else if (uRR.indexOf(('Time:').toUpperCase()) === 3) {
-                ret.time = global.moment(rr.split(/\s+/g)[2]).format().toString();
+                ret.time = rr.split(/\s+/g)[2]);
             } else if (uRR.indexOf(('Finished zone:').toUpperCase()) === 3) {
-                var totalStr = "634343 nodes and 8234234 records in 2342340 seconds".replace(/([0-9]*)( nodes.*?)([0-9]*)( records.*?)([0-9]*)( seconds.*?)/gi, "$1-$3-$5");
+                var totalStr = rr.replace(/([0-9]*)( nodes.*?)([0-9]*)( records.*?)([0-9]*)( seconds.*?)/gi, "$1-$3-$5");
                 ret.nodes = isNaN(parseInt(totalStr.split('-')[0], 10)) ? -1 : parseInt(totalStr.split('-')[0], 10);
                 ret.records = isNaN(parseInt(totalStr.split('-')[1], 10)) ? -1 : parseInt(totalStr.split('-')[1], 10);
                 ret.seconds = isNaN(parseInt(totalStr.split('-')[2], 10)) ? -1 : parseInt(totalStr.split('-')[2], 10);
